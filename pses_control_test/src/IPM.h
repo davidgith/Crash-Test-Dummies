db7d52ab8f3c57890448c796d543062695a05e1a
davidgith <davidbotschek@gmx.de>
2018-12-13 13:26:16 +0100

    Reduzierung der Header-Datei

--- pses_control_test/src/IPM.h
+++ pses_control_test/src/IPM.h
@@ -16,21 +16,7 @@ public:
 	
 	// Apply IPM on points
 	cv::Point2d applyHomography(const cv::Point2d& _point, const cv::Mat& _H);
-	cv::Point3d applyHomography( const cv::Point3d& _point, const cv::Mat& _H);
-	cv::Point2d applyHomography(const cv::Point2d& _point);
-	cv::Point3d applyHomography( const cv::Point3d& _point);
-	cv::Point2d applyHomographyInv(const cv::Point2d& _point);
-	cv::Point3d applyHomographyInv( const cv::Point3d& _point);
 	void applyHomography( const cv::Mat& _origBGR, cv::Mat& _ipmBGR, int borderMode = cv::BORDER_CONSTANT);
-	void applyHomographyInv( const cv::Mat& _ipmBGR, cv::Mat& _origBGR, int borderMode = cv::BORDER_CONSTANT);
-
-	// Getters
-	cv::Mat getH() const { return m_H; }
-	cv::Mat getHinv() const { return m_H_inv; }
-	void getPoints(std::vector<cv::Point2f>& _origPts, std::vector<cv::Point2f>& _ipmPts);
-
-	// Draw
-	void drawPoints( const std::vector<cv::Point2f>& _points, cv::Mat& _img ) const; 
 
 private:
 	void createMaps();
@@ -49,7 +35,6 @@ private:
 
 	// Maps	
 	cv::Mat m_mapX, m_mapY;
-	cv::Mat m_invMapX, m_invMapY;
 };
 
 #endif /*__IPM_H__*/
